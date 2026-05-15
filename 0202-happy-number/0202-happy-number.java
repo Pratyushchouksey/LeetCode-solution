b077e90575c2caa1321c2087a3167708a46203df
Pratyush Chouksey <pratyushchouksey214@gmail.com>
2026-05-16 02:08:15 +0530

    Time: 1 ms (80.65%), Space: 42.4 MB (66.58%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while(fast!=1)
+        {
+           slow = squareSum(slow);
+           fast = squareSum(squareSum(fast));
+
+            if(slow==fast && slow!=1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+   public int squareSum(int n) 
+        {
+            int sum=0;
+            while(n>0){
+                int d = n%10;
+                n = n/10;
+                sum = sum + d * d;
+            }
+            return sum;
+        }
+}
