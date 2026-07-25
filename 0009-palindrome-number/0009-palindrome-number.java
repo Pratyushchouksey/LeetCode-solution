bee8b1928ecee564091ed58db5077ebd20ea9a07
Pratyush Chouksey <pratyushchouksey214@gmail.com>
2026-07-26 00:53:55 +0530

    Time: 5 ms (80.95%), Space: 45.9 MB (72.55%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,39 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // Negative number kabhi palindrome nahi hota
-        if (x < 0) {
+        if(x<0)
+        {
             return false;
+        }    
+
+        int n = x;
+        int rev = 0;
+
+        while(n>0)
+        {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n /10;
         }
-
-        int divisor = 1;
-
-        // Left pointer ke liye divisor banana
-        while (x / divisor >= 10) {
-            divisor *= 10;
-        }
-
-        while (x != 0) {
-
-            // Left digit
-            int left = x / divisor;
-
-            // Right digit
-            int right = x % 10;
-
-            if (left != right) {
-                return false;
-            }
-
-            // Left digit remove
-            x = x % divisor;
-
-            // Right digit remove
-            x = x / 10;
-
-            // Dono pointers andar aaye
-            divisor = divisor / 100;
-        }
-
-        return true;
+        return rev==x;
     }
 }
